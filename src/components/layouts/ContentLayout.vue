66b0c3068d5e1537976e8ecee4a285a4fc0311bf
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 12:29:23 -0300

    fix: rename content layout

---
+++ src/components/layouts/ContentLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import ContainerCard from '@/components/atoms/containers/ContainerCard.vue'
+</script>
+
+<template>
+  <div class="content">
+    <ContainerCard>
+      <h1>content</h1>
+    </ContainerCard>
+
+    <ContainerCard>
+      <h1>content</h1>
+    </ContainerCard>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .content {
+    border: 2px solid red;
+    overflow-y: auto;
+  }
+</style>
