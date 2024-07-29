2592033f3e5ecf57c791c2fd33daaf51b4766eb5
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 12:26:31 -0300

    feat: create card container component

---
+++ src/components/atoms/containers/ContainerCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container-card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .container-card {
+    background-color: $gray-200;
+    border-radius: $border-radius-width;
+    padding: 24px;
+  }
+</style>
