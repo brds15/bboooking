2a67c93eaa814f5b16c1ac005e616f3ce1aadef2
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 12:29:39 -0300

    fix: rename header layout

---
+++ src/components/layouts/HeaderLayout.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+  import IconNotifications from '@/components/atoms/icons/IconNotifications.vue'
+  import IconMap from '@/components/atoms/icons/IconMap.vue'
+  import IconLuggage from '@/components/atoms/icons/IconLuggage.vue'
+</script>
+
+<template>
+  <header class="header">
+    <div class="header-icon">
+      <img width="40" height="40" alt="Vue logo" src="../../assets/images/logo.png" />
+      <h1>BBoooKing</h1>
+    </div>
+    <div class="header-options">
+      <IconMap classes="checked" />
+      <IconLuggage />
+    </div>
+    <div class="header-profile">
+      <span>Sem notificações</span>
+      <IconNotifications />
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  .header {
+    border: 2px solid green;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 4px 0;
+    width: 100%;
+
+    div {
+      align-items: center;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    &-icon {
+      img {
+        margin-right: 12px;
+      }
+
+      h1 {
+        color: $black-800;
+        font-size: 1.8em;
+        font-weight: 700;
+      }
+    }
+
+    &-options {
+      gap: 32px;
+    }
+
+    &-profile {
+      span {
+        color: $black-800;
+        font-size: 0.8em;
+        font-weight: 500;
+        margin-right: 12px;
+      }
+    }
+  }
+</style>
