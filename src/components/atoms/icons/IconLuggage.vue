49d73be1d95241df17adaa76fc6911e62fb9f005
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 12:24:25 -0300

    feat: create icons components

---
+++ src/components/atoms/icons/IconLuggage.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1.7em" height="1.7em" viewBox="0 0 14 14">
+    <g fill="none" stroke="#222c40" stroke-linecap="round" stroke-linejoin="round">
+      <rect width="13" height="9.5" x=".5" y="4" rx="2"></rect>
+      <path d="M4 13.5V4m6 9.5V4M4.5 4a2.5 2.5 0 0 1 5 0"></path>
+    </g>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'IconLuggage'
+  }
+</script>
