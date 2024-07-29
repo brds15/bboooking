c68b0fcd2804f7d94acbe26cbb3bc6e94d5f93ed
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 12:30:20 -0300

    feat: define struct page layouts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <img alt="Vue logo" src="@/assets/images/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HeaderLayout />
+  <ContentLayout />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from './components/HelloWorld.vue'
+  import { Options, Vue } from 'vue-class-component'
+  import ContentLayout from '@/components/layouts/ContentLayout.vue'
+  import HeaderLayout from '@/components/layouts/HeaderLayout.vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+  @Options({
+    components: {
+      HeaderLayout,
+      ContentLayout
+    }
+  })
+  export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-  @import "@/styles/default.scss";
+  @import '@/styles/default.scss';
 </style>
