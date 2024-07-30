b5b4896d9a09d83b96b0794573652f6b40db105b
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-07-29 23:37:06 -0300

    feat: create button component

---
+++ src/components/atoms/buttons/ButtonPrimary.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'ButtonPrimary',
+    props: {
+      text: {
+        type: String,
+        required: true
+      },
+      size: {
+        default: 'regular',
+        required: false,
+        type: String
+      }
+    },
+    setup(props) {
+      return {
+        props
+      }
+    }
+  })
+</script>
+
+<template>
+  <button :class="['button', props.size]" @click="$emit('click')">{{ props.text }}</button>
+</template>
+
+<style scoped lang="scss">
+  .button {
+    &:hover {
+      background-color: $green-500;
+    }
+
+    background-color: $green-400;
+    border-radius: 0.5rem;
+    color: white;
+    cursor: pointer;
+    font-size: 0.875rem;
+    font-weight: 500;
+    line-height: 1.25rem;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+  }
+</style>
