89a9058afb604ca2b004bf8036bdf19b09e0bb4a
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-01 16:57:37 -0300

    feat: create BookHotelInfo

---
+++ src/components/organisms/Book/BookHotelInfo.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import ContainerCard from '@/components/atoms/containers/ContainerCard.vue'
+  import HotelDetailCard from '@/components/molecules/Hotel/HotelDetailCard.vue'
+
+  export default defineComponent({
+    name: 'BookHotelInfo',
+    components: { ContainerCard, HotelDetailCard }
+  })
+</script>
+
+<template>
+  <ContainerCard classes="book-hotel-info">
+    <HotelDetailCard :onlyViewer="true" />
+  </ContainerCard>
+</template>
+
+<style scoped lang="scss">
+  .book-hotel-info {
+    width: 49%;
+
+    @media (max-width: 992px) {
+      width: 100%;
+    }
+  }
+</style>
