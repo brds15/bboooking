e072c1eacb982fa3a1810a54f76b2cdb117455fc
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-04 22:46:38 -0300

    feat: create interceptor delay

--- src/services/api/configuration/apiInstances.ts
+++ src/services/api/configuration/apiInstances.ts
@@ -2,6 +2,21 @@ import axios from 'axios'
 
 import { API_URL } from '@/constants/routes'
 
+const addDelay = (): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 1800)
+  })
+
 const apiInstance = axios.create({ baseURL: API_URL })
+
+apiInstance.interceptors.response.use(async response => {
+  if (process.env.NODE_ENV === 'development') {
+    await addDelay()
+  }
+
+  return response
+})
 
 export default apiInstance
